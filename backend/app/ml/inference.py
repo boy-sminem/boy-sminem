0ca85b79c9aed0dc63bbc5e3d42858ce8872ed72
Артём Соседка <sostema@visionary.local>
2022-04-17 12:29:11 +0300

    🤯 implemented backend-side inference

---
+++ backend/app/ml/inference.py
@@ -0,0 +1,42 @@
+import timm
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+
+SIZE = 224
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+
+device = torch.device(f"cuda" if torch.cuda.is_available() else "cpu")
+
+
+class ModelInference():
+    def __init__(self, model_name="mobilenetv2_140", path_to_weights="mobilenetv2_140_best.ckpt"):
+        self.model = timm.create_model(model_name=model_name, pretrained=False, num_classes=40)
+        cp = torch.load(path_to_weights)
+        state_dict = cp["state_dict"]
+        state_dict = {k.replace("net.", ""): v for k, v in state_dict.items()}
+        self.model.load_state_dict(state_dict)
+
+        self.model.to(device)
+        self.model.eval()
+
+    def image_inference(self, img):
+        inference_transforms = transforms.Compose(
+            [
+                transforms.Resize(SIZE),
+                transforms.ToTensor(),
+                transforms.Normalize(MEAN, STD),
+            ]
+        )
+
+        image = Image.fromarray(img)
+        image = inference_transforms(image).float()
+        image_tensor = image.unsqueeze_(0)
+        input = image_tensor.to(device)
+
+        output = self.model(input)
+
+        probs = torch.nn.functional.softmax(output, dim=1).data.cpu().numpy()
+
+        return probs
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,10 +1,29 @@
-from fastapi import APIRouter, WebSocket
+import base64
 
+from fastapi import APIRouter, WebSocket
+from starlette.concurrency import run_in_threadpool
+
+from backend.app.ml.constant import ATTRIBUTES
+from backend.app.ml.inference import ModelInference
 
 router = APIRouter()
+
+model = ModelInference("./ml/mobilenetv2_140_best.ckpt")
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    await websocket.send_json({"ping": "pong"})
+
+    while True:
+        try:
+            data = await websocket.receive_json()
+            response = {"person_id": data["person_id"], "result": []}
+            image = base64.decodebytes(data["img"])
+            probs = await run_in_threadpool(model.image_inference, image)
+            for k, v in ATTRIBUTES.items():
+                response["result"].append({"attribute_name": v.lower(), "data": probs[k]})
+            await websocket.send_json(response)
+
+        except Exception as e:
+            break
